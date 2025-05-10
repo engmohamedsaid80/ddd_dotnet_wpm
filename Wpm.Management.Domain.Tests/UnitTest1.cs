1a1a823b7c8eed96b81e548871960867b7ce4d97
engmohamedsaid80 <eng.mohamed.said80@gmail.com>
2025-05-10 14:14:42 +0200

    Pet entity and unit tests

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -3,8 +3,45 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Pet_should_be_equal()
     {
+        // Arrange
+        var id = Guid.NewGuid();
+        var pet1 = new Pet() { Id = id };
+        var pet2 = new Pet() { Id = id };
 
+        //Act
+
+        // Assert
+        Assert.True(pet1.Equals(pet2));
+    }
+
+    [Fact]
+    public void Pet_should_be_equal_using_operators()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var pet1 = new Pet() { Id = id };
+        var pet2 = new Pet() { Id = id };
+
+        //Act
+
+        // Assert
+        Assert.True(pet1 == pet2);
+    }
+
+    [Fact]
+    public void Pet_should_be_not_equal_using_operators()
+    {
+        // Arrange
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        var pet1 = new Pet() { Id = id1 };
+        var pet2 = new Pet() { Id = id2 };
+
+        //Act
+
+        // Assert
+        Assert.True(pet1 != pet2);
     }
 }
---
+++ Wpm.Management.Domain/Pet.cs
@@ -0,0 +1,33 @@
+﻿namespace Wpm.Management.Domain;
+
+public class Pet : IEquatable<Pet>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    bool IEquatable<Pet>.Equals(Pet? other)
+    {
+        return other?.Id == Id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return ((IEquatable<Pet>)this).Equals(obj as Pet);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Pet left, Pet right)
+    {
+        return left?.Id == right?.Id;
+    }
+
+    public static bool operator !=(Pet left, Pet right)
+    {
+        return left?.Id != right?.Id;
+    }
+}
