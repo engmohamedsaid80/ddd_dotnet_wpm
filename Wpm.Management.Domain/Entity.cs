d8ff0454d99505815d131b0a44bd6e202fc0fc67
engmohamedsaid80 <eng.mohamed.said80@gmail.com>
2025-05-10 14:33:15 +0200

    refactor Pet class with base entity

---
+++ Wpm.Management.Domain/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace Wpm.Management.Domain;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; set; }
+
+    bool IEquatable<Entity>.Equals(Entity? other)
+    {
+        return other?.Id == Id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return ((IEquatable<Entity>)this).Equals(obj as Entity);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity left, Entity right)
+    {
+        return left?.Id == right?.Id;
+    }
+
+    public static bool operator !=(Entity left, Entity right)
+    {
+        return left?.Id != right?.Id;
+    }
+}
--- Wpm.Management.Domain/Pet.cs
+++ Wpm.Management.Domain/Pet.cs
@@ -1,33 +1,8 @@
 ﻿namespace Wpm.Management.Domain;
-
-public class Pet : IEquatable<Pet>
+public class Pet : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
 
-    bool IEquatable<Pet>.Equals(Pet? other)
-    {
-        return other?.Id == Id;
-    }
-
-    public override bool Equals(object obj)
-    {
-        return ((IEquatable<Pet>)this).Equals(obj as Pet);
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
-
-    public static bool operator ==(Pet left, Pet right)
-    {
-        return left?.Id == right?.Id;
-    }
-
-    public static bool operator !=(Pet left, Pet right)
-    {
-        return left?.Id != right?.Id;
-    }
+    
 }
