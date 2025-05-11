a3ebc2c0680026e029069908219526fbbbf65839
engmohamedsaid80 <eng.mohamed.said80@gmail.com>
2025-05-11 21:37:21 +0200

    Breed entity

---
+++ Wpm.Management.Domain/Breed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpm.Management.Domain;
+
+public class Breed : Entity
+{
+    public string Name { get; init; }
+    public WeightRange MaleIdealWeight { get; init; }
+    public WeightRange FemaleIdealWeight { get; init; }
+    public Breed(string name,
+                 WeightRange maleIdealWeight,
+                 WeightRange femaleIdealWeight)
+    {
+        Name = name;
+        MaleIdealWeight = maleIdealWeight;
+        FemaleIdealWeight = femaleIdealWeight;
+    }
+}
---
+++ Wpm.Management.Domain/WeightRange.cs
@@ -0,0 +1,13 @@
+﻿namespace Wpm.Management.Domain;
+
+public record WeightRange
+{
+    public decimal From { get; init; }
+    public decimal To { get; init; }
+
+    public WeightRange(decimal from, decimal to)
+    {
+        From = from;
+        To = to;
+    }
+}
