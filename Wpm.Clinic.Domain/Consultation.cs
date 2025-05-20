fa38cf3c5950f1b96ca19bf2977668fc614360df
engmohamedsaid80 <eng.mohamed.said80@gmail.com>
2025-05-20 23:10:40 +0200

    Consultation logic

--- Wpm.Clinic.Domain/Consultation.cs
+++ Wpm.Clinic.Domain/Consultation.cs
@@ -5,6 +5,8 @@ namespace Wpm.Clinic.Domain;
 
 public class Consultation : AggregateRoot
 {
+    public DateTime StartedAt { get; init; }
+    public DateTime? EndedAt { get; private set; }
     public Text Diagnosis { get; private set; }
     public Text Treatment { get; private set; }
 
@@ -14,9 +16,46 @@ public class Consultation : AggregateRoot
 
     public ConsultationStatus Status { get; private set; }
 
+    public Consultation(PaitentId paitentId)
+    {
+        Id = Guid.NewGuid();
+        PaitentId = paitentId;
+        Status = ConsultationStatus.Open;
+        StartedAt = DateTime.UtcNow;
+    }
+
+    public void End()
+    {
+        ValidateConsultationStatus();
+
+        if (Diagnosis == null || Treatment == null || CurrentWeight == null)
+            throw new InvalidOperationException("Cannot close a consultation due to missing data.");
+        
+        Status = ConsultationStatus.Closed;
+        EndedAt = DateTime.UtcNow;
+    }
     public void SetWeight(Weight weight)
     {
+        ValidateConsultationStatus();
         CurrentWeight = weight;
+    }
+
+    public void SetDiagnosis(Text diagnosis)
+    {
+        ValidateConsultationStatus();
+        Diagnosis = diagnosis;
+    }
+
+    public void SetTreatment(Text treatment)
+    {
+        ValidateConsultationStatus();
+        Treatment = treatment;
+    }
+
+    private void ValidateConsultationStatus()
+    {
+        if(Status != ConsultationStatus.Open)
+            throw new InvalidOperationException("Cannot change a closed or canceled consultation.");
     }
 }
 
